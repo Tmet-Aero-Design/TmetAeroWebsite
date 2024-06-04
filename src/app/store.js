6181f558cba112a59902724b0064f98aa4d47b02
NinePiece2 <romit.sagu@hotmail.com>
2024-06-04 09:55:11 -0400

    Updates

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-// src/app/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import portfolioReducer from '../features/portfolio/portfolioSlice';
-
-export const store = configureStore({
-  reducer: {
-    portfolio: portfolioReducer,
-  },
-});
